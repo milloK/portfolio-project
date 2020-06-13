1d33275b9080f9a70614000e009a6c62501a6807
millo kuzman <millokuzman@gmail.com>
2020-06-13 18:16:28 +0200

    scatic_root change

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -139,6 +139,6 @@ STATICFILES_DIRS = [
 ]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 #STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-#MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-#MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 django_heroku.settings(locals())
